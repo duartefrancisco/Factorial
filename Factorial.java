9f78026cd6b795239eb25da43f1e24178b2a7975
Francisco Duarte <francisco.duarte@galileo.edu>
2018-01-28 12:24:29 -0600

    Se agregó código al archivo

---
+++ Factorial.java
@@ -0,0 +1,10 @@
+class Factorial {
+	public staticvoid main(String args[]){
+		int i, fact = 1;
+		int number = 5;
+		for(i = 1; i <= number; i++){
+			fact = fact *i;
+		}
+		System.out.println("Factorial of" + number + "is: " + fact);
+	}
+}
